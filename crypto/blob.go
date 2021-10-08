275d90d665d44133a053728b8aa8e8552b4f4f39
Steven Penny <srpen6@gmail.com>
2021-10-08 16:42:38 -0500

    -func (b *BlobInfo) SaveToFile(path string) error {

--- crypto/blob.go
+++ crypto/blob.go
@@ -7,13 +7,9 @@ import (
    "crypto/hmac"
    "crypto/sha1"
    "encoding/base64"
-   "encoding/binary"
-   "encoding/json"
    "errors"
-   "golang.org/x/crypto/pbkdf2"
    "log"
    "math/big"
-   "os"
 )
 
 // BlobInfo is the structure holding authentication blob data. The blob is an
@@ -22,87 +18,6 @@ import (
 type BlobInfo struct {
 	Username    string
 	DecodedBlob string
-}
-
-// SaveToFile saves the current blob to the specified path
-func (b *BlobInfo) SaveToFile(path string) error {
-	file, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-
-	defer file.Close()
-
-	encoder := json.NewEncoder(file)
-	err = encoder.Encode(b)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func blobKey(username string, secret []byte) []byte {
-	data := pbkdf2.Key(secret, []byte(username), 0x100, 20, sha1.New)[0:20]
-
-	hash := sha1.Sum(data)
-	length := make([]byte, 4)
-	binary.BigEndian.PutUint32(length, 20)
-	return append(hash[:], length...)
-}
-
-func makeBlob(blobPart []byte, keys PrivateKeys, publicKey string) string {
-	part := []byte(base64.StdEncoding.EncodeToString(blobPart))
-
-	sharedKey := keys.SharedKey(publicKey)
-	iv := RandomVec(16)
-
-	key := sha1.Sum(sharedKey)
-	base_key := key[:16]
-	hash := hmac.New(sha1.New, base_key)
-
-	hash.Write([]byte("checksum"))
-	checksum_key := hash.Sum(nil)
-	hash.Reset()
-
-	hash.Write([]byte("encryption"))
-	encryption_key := hash.Sum(nil)
-	hash.Reset()
-
-	block, _ := aes.NewCipher(encryption_key[0:16])
-	stream := cipher.NewCTR(block, iv)
-	stream.XORKeyStream(part, part)
-
-	macHash := hmac.New(sha1.New, checksum_key)
-	macHash.Write(part)
-	mac := macHash.Sum(nil)
-
-	part = append(iv, part...)
-	part = append(part, mac...)
-
-	return base64.StdEncoding.EncodeToString(part)
-}
-
-func encryptBlob(blob []byte, key []byte) []byte {
-	block, _ := aes.NewCipher(key)
-	bs := block.BlockSize()
-	if len(blob)%bs != 0 {
-		panic("Need a multiple of the blocksize")
-	}
-
-	l := len(blob)
-	for i := l - 0x11; i >= 0; i-- {
-		blob[l-i-1] = blob[l-i-1] ^ blob[l-i-0x11]
-	}
-
-	ciphertext := make([]byte, len(blob))
-	encoded := ciphertext
-	for len(blob) > 0 {
-		block.Encrypt(ciphertext, blob)
-		ciphertext = ciphertext[bs:]
-		blob = blob[bs:]
-	}
-
-	return encoded
 }
 
 func decodeBlob(blob64 string, client64 string, keys PrivateKeys) (string, error) {
@@ -150,15 +65,6 @@ func decodeBlob(blob64 string, client64 string, keys PrivateKeys) (string, error
 	stream.XORKeyStream(encryptedPart, encryptedPart)
 
 	return string(encryptedPart), nil
-}
-
-func decodeBlobSecondary(blob64 string, username string, deviceId string) []byte {
-	blob, _ := base64.StdEncoding.DecodeString(blob64)
-	secret := sha1.Sum([]byte(deviceId))
-	key := blobKey(username, secret[:])
-
-	data := decryptBlob(blob, key)
-	return data
 }
 
 func decryptBlob(blob []byte, key []byte) []byte {
