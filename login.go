c74a06709b874bdd1ed536f9a1ad7b9bf234a326
Steven Penny <srpen6@gmail.com>
2021-10-08 18:38:55 -0500

    -func CoreLoginOAuth(deviceName string, clientId string, clientSecret string) (*Session, error) {

--- login.go
+++ login.go
@@ -1,15 +1,11 @@
 package spotify
 
 import (
-   "encoding/json"
    "fmt"
    "github.com/89z/spotify/pb"
    "github.com/89z/spotify/crypto"
    "github.com/golang/protobuf/proto"
-   "io/ioutil"
    "log"
-   "net/http"
-   "net/url"
 )
 
 var Version = "master"
@@ -54,30 +50,6 @@ func CoreLoginSaved(username string, authData []byte, deviceName string) (*Sessi
 	packet := makeLoginBlobPacket(username, authData,
 		pb.AuthenticationType_AUTHENTICATION_STORED_SPOTIFY_CREDENTIALS.Enum(), s.deviceId)
 	return s, s.doLogin(packet, username)
-}
-
-func CoreLoginOAuth(deviceName string, clientId string, clientSecret string) (*Session, error) {
-	token := getOAuthToken(clientId, clientSecret)
-	return loginOAuthToken(token.AccessToken, deviceName)
-}
-
-func loginOAuthToken(accessToken string, deviceName string) (*Session, error) {
-	s, err := setupSession()
-	if err != nil {
-		return s, err
-	}
-
-	s.deviceId = GenerateDeviceId(deviceName)
-	s.deviceName = deviceName
-
-	err = s.startConnection()
-	if err != nil {
-		return s, err
-	}
-
-	packet := makeLoginBlobPacket("", []byte(accessToken),
-		pb.AuthenticationType_AUTHENTICATION_SPOTIFY_TOKEN.Enum(), s.deviceId)
-	return s, s.doLogin(packet, "")
 }
 
 // NEED THIS
@@ -175,75 +147,4 @@ func makeLoginBlobPacket(username string, authData []byte,
 	}
 
 	return packetData
-}
-
-
-type OAuth struct {
-	AccessToken  string `json:"access_token"`
-	RefreshToken string `json:"refresh_token"`
-	Scope        string `json:"scope"`
-	Error        string
-}
-
-func GetOauthAccessToken(code string, redirectUri string, clientId string, clientSecret string) (*OAuth, error) {
-	val := url.Values{}
-	val.Set("grant_type", "authorization_code")
-	val.Set("code", code)
-	val.Set("redirect_uri", redirectUri)
-	val.Set("client_id", clientId)
-	val.Set("client_secret", clientSecret)
-
-	resp, err := http.PostForm("https://accounts.spotify.com/api/token", val)
-	if err != nil {
-		// Retry since there is an nginx bug that causes http2 streams to get
-		// an initial REFUSED_STREAM response
-		// https://github.com/curl/curl/issues/804
-		resp, err = http.PostForm("https://accounts.spotify.com/api/token", val)
-		if err != nil {
-			return nil, err
-		}
-	}
-	defer resp.Body.Close()
-	auth := OAuth{}
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	err = json.Unmarshal(body, &auth)
-	if err != nil {
-		return nil, err
-	}
-	if auth.Error != "" {
-		return nil, fmt.Errorf("error getting token %v", auth.Error)
-	}
-	return &auth, nil
-}
-
-func getOAuthToken(clientId string, clientSecret string) OAuth {
-	ch := make(chan OAuth)
-
-	fmt.Println("go to this url")
-	urlPath := "https://accounts.spotify.com/authorize?" +
-		"client_id=" + clientId +
-		"&response_type=code" +
-		"&redirect_uri=http://localhost:8888/callback" +
-		"&scope=streaming"
-	fmt.Println(urlPath)
-
-	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
-		params := r.URL.Query()
-		auth, err := GetOauthAccessToken(params.Get("code"), "http://localhost:8888/callback", clientId, clientSecret)
-		if err != nil {
-			fmt.Fprintf(w, "Error getting token %q", err)
-			return
-		}
-		fmt.Fprintf(w, "Got token, loggin in")
-		ch <- *auth
-	})
-
-	go func() {
-		log.Fatal(http.ListenAndServe(":8888", nil))
-	}()
-
-	return <-ch
 }
