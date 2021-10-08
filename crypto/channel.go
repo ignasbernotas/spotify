57593a5768dbeda0d3eab4589ea62570d7fea2fe
Steven Penny <srpen6@gmail.com>
2021-10-08 16:44:20 -0500

    deleted:    crypto/blob.go

--- crypto/channel.go
+++ crypto/channel.go
@@ -1,13 +1,17 @@
-//package player
 package crypto
 
 import (
-	"bytes"
-	"crypto/aes"
-	"crypto/cipher"
-	"encoding/binary"
-	"math"
-	"math/big"
+   "bytes"
+   "crypto/aes"
+   "crypto/cipher"
+   "crypto/hmac"
+   "crypto/sha1"
+   "encoding/base64"
+   "encoding/binary"
+   "errors"
+   "log"
+   "math"
+   "math/big"
 )
 
 type headerFunc func(channel *Channel, id byte, data *bytes.Reader) uint16
@@ -168,4 +172,84 @@ func (afd *AudioFileDecrypter) DecryptAudioWithBlock(index int, block cipher.Blo
 	}
 
 	return plaintext[0:length]
+}
+
+
+// BlobInfo is the structure holding authentication blob data. The blob is an
+// encoded/encrypted byte array (encoded as base64), holding the encryption
+// keys, the deviceId, and the username.
+type BlobInfo struct {
+	Username    string
+	DecodedBlob string
+}
+
+func decodeBlob(blob64 string, client64 string, keys PrivateKeys) (string, error) {
+
+	clientKey, err := base64.StdEncoding.DecodeString(client64)
+	if err != nil {
+		return "", err
+	}
+
+	blobBytes, err := base64.StdEncoding.DecodeString(blob64)
+	if err != nil {
+		return "", err
+	}
+
+	clientKey_be := new(big.Int)
+	clientKey_be.SetBytes(clientKey)
+
+	sharedKey := Powm(clientKey_be, keys.PrivateKey(), keys.Prime())
+	iv := blobBytes[0:16]
+	encryptedPart := blobBytes[16 : len(blobBytes)-20]
+	ckSum := blobBytes[len(blobBytes)-20:]
+	key := sha1.Sum(sharedKey.Bytes())
+	base_key := key[:16]
+	hash := hmac.New(sha1.New, base_key)
+
+	hash.Write([]byte("checksum"))
+	checksum_key := hash.Sum(nil)
+	hash.Reset()
+
+	hash.Write([]byte("encryption"))
+	encryption_key := hash.Sum(nil)
+	hash.Reset()
+
+	macHash := hmac.New(sha1.New, checksum_key)
+	macHash.Write(encryptedPart)
+	mac := macHash.Sum(nil)
+
+	if !bytes.Equal(mac, ckSum) {
+		log.Println("add user error, mac doesn't match")
+		return "", errors.New("mac mismatch")
+	}
+
+	block, _ := aes.NewCipher(encryption_key[0:16])
+	stream := cipher.NewCTR(block, iv)
+	stream.XORKeyStream(encryptedPart, encryptedPart)
+
+	return string(encryptedPart), nil
+}
+
+func decryptBlob(blob []byte, key []byte) []byte {
+	block, _ := aes.NewCipher(key)
+	bs := block.BlockSize()
+	if len(blob)%bs != 0 {
+		panic("Need a multiple of the blocksize")
+	}
+
+	plaintext := make([]byte, len(blob))
+
+	plain := plaintext
+	for len(blob) > 0 {
+		block.Decrypt(plaintext, blob)
+		plaintext = plaintext[bs:]
+		blob = blob[bs:]
+	}
+
+	l := len(plain)
+	for i := 0; i < l-0x10; i++ {
+		plain[l-i-1] = plain[l-i-1] ^ plain[l-i-0x11]
+	}
+
+	return plain
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,6 @@ module github.com/89z/spotify
 
 go 1.17
 
-require (
-	github.com/golang/protobuf v1.5.2
-	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
-)
+require github.com/golang/protobuf v1.5.2
 
 require google.golang.org/protobuf v1.26.0 // indirect
