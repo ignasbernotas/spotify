b0f908d3dc8837e96f6d871e79a31a079eda68c0
Steven Penny <srpen6@gmail.com>
2021-10-08 16:37:39 -0500

    deleted:    crypto/api.go

--- crypto/metadata.go
+++ crypto/metadata.go
@@ -1,7 +1,10 @@
 package crypto
 
 import (
-	"encoding/json"
+   "encoding/json"
+   "fmt"
+   "github.com/golang/protobuf/proto"
+   "github.com/89z/spotify/pb"
 )
 
 type Artist struct {
@@ -157,4 +160,74 @@ type Token struct {
 	ExpiresIn   int      `json:"expiresIn"`
 	TokenType   string   `json:"tokenType"`
 	Scope       []string `json:"scope"`
+}
+
+
+func (m *Client) mercuryGet(url string) []byte {
+	done := make(chan []byte)
+	go m.Request(Request{
+		Method:  "GET",
+		Uri:     url,
+		Payload: [][]byte{},
+	}, func(res Response) {
+		done <- res.CombinePayload()
+	})
+
+	result := <-done
+	return result
+}
+
+func (m *Client) mercuryGetJson(url string, result interface{}) (err error) {
+	data := m.mercuryGet(url)
+	// fmt.Printf("Received:\n%s\n\n\n", data)
+	err = json.Unmarshal(data, result)
+	return
+}
+
+func (m *Client) mercuryGetProto(url string, result proto.Message) error {
+   data := m.mercuryGet(url)
+   return proto.Unmarshal(data, result)
+}
+
+func (m *Client) GetTrack(id string) (*pb.Track, error) {
+	uri := "hm://metadata/4/track/" + id
+	result := &pb.Track{}
+	err := m.mercuryGetProto(uri, result)
+	return result, err
+}
+
+func (m *Client) GetAlbumInfo(id string, username string) (*DetailPageGenericRelease, error) {
+	uri := fmt.Sprintf("hm://album/v1/album-app/album/spotify:album:%s/desktop?catalogue=free&locale=en&username=%s", id, username)
+
+	result := &DetailPageGenericRelease{}
+	err := m.mercuryGetJson(uri, result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func parseSuggest(body []byte) (*SuggestResult, error) {
+	result := &SuggestResult{}
+	err := json.Unmarshal(body, result)
+	if err != nil {
+		fmt.Println("err", err)
+	}
+
+	for _, s := range result.Sections {
+		switch s.Typ {
+		case "top-results":
+			err = json.Unmarshal(s.RawItems, &result.TopHits)
+		case "album-results":
+			err = json.Unmarshal(s.RawItems, &result.Albums)
+		case "artist-results":
+			err = json.Unmarshal(s.RawItems, &result.Artists)
+		case "track-results":
+			err = json.Unmarshal(s.RawItems, &result.Tracks)
+		}
+		if err != nil {
+			return nil, err
+		}
+	}
+	return result, nil
 }
--- spotify/spotify.go
+++ spotify/spotify.go
@@ -2,7 +2,6 @@ package main
 
 import (
    "flag"
-   "fmt"
    "github.com/89z/spotify"
    "log"
    "strings"
@@ -29,7 +28,6 @@ func main() {
 	username := flag.String("username", EMPTY_STIRNG, "Username of the spotify account. Required.")
 	password := flag.String("password", EMPTY_STIRNG, "Password of the spotify account. Required.")
 	tracks := flag.String("tracks", EMPTY_STIRNG, "List of track IDs to download, separated by commas.")
-	artistInfo := flag.String("artist_info", EMPTY_STIRNG, "Specify an artist ID in this field to get JSON formatted information about that artist.")
 
 	flag.Parse()
 	if !isStringDefined(*username) || !isStringDefined(*password) {
@@ -45,12 +43,5 @@ func main() {
 		if err != nil {
 			log.Fatalf("Failed to download tracks: %+v", err)
 		}
-	}
-	if isStringDefined(*artistInfo) {
-		response, err := session.Mercury().GetArtistInfo(*artistInfo, session.Username())
-		if err != nil {
-			log.Fatalf("Failed to get artist info for artist %s: %+v", *artistInfo, err)
-		}
-		fmt.Print(spotify.NiceJsonFormat(response))
 	}
 }
