2c53d40fc7976e4afb2b3c46acefb0738d7aa178
Steven Penny <srpen6@gmail.com>
2021-10-11 12:48:40 -0500

    +   req, err := http.NewRequest("GET", "http://apresolve.spotify.com", nil)

--- keys.go
+++ keys.go
@@ -6,43 +6,35 @@ import (
    "encoding/base64"
    "encoding/json"
    "errors"
+   "fmt"
    "github.com/89z/spotify/pb"
    "github.com/golang/protobuf/proto"
    "log"
    "math/big"
    "math/rand"
    "net/http"
-   "strconv"
    "strings"
-   "time"
    cryptoRand "crypto/rand"
 )
 
-const (
-   alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-   kAPEndpoint = "https://apresolve.spotify.com"
-)
-
-var empty_HTTP_RESPONSE = &http.Response{}
-
-var standard_APRESOLVE_HEADERS = map[string]string{
-   "User-Agent":                        "Spotify/111000546 (8; 0; 5)",
-   "x-spotify-ap-resolve-pod-override": "0",
-}
+const alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func apResolve() (string, error) {
-   var unixTimestamp = strconv.Itoa(int(time.Now().Unix()))
-   r, err := httpGetHeaders(
-      kAPEndpoint+"/?time="+unixTimestamp+"&type=accesspoint",
-      standard_APRESOLVE_HEADERS,
-   )
+   req, err := http.NewRequest("GET", "http://apresolve.spotify.com", nil)
    if err != nil {
-   return "", err
+      return "", err
    }
-   defer r.Body.Close()
-   var endpoints apList
-   err = json.NewDecoder(r.Body).Decode(&endpoints)
+   val := req.URL.Query()
+   val.Set("type", "accesspoint")
+   req.URL.RawQuery = val.Encode()
+   fmt.Println(req.Method, req.URL)
+   res, err := new(http.Client).Do(req)
    if err != nil {
+      return "", err
+   }
+   defer res.Body.Close()
+   var endpoints apList
+   if err := json.NewDecoder(res.Body).Decode(&endpoints); err != nil {
       return "", err
    }
    if len(endpoints.ApList) > 0 {
@@ -55,45 +47,24 @@ func apResolve() (string, error) {
 }
 
 func convert62(id string) []byte {
-	base := big.NewInt(62)
-
-	n := &big.Int{}
-	for _, c := range []byte(id) {
-		d := big.NewInt(int64(strings.IndexByte(alphabet, c)))
-		n = n.Mul(n, base)
-		n = n.Add(n, d)
-	}
-
-	nBytes := n.Bytes()
-	if len(nBytes) < 16 {
-		paddingBytes := make([]byte, 16-len(nBytes))
-		nBytes = append(paddingBytes, nBytes...)
-	}
-	return nBytes
+   base := big.NewInt(62)
+   n := &big.Int{}
+   for _, c := range []byte(id) {
+      d := big.NewInt(int64(strings.IndexByte(alphabet, c)))
+      n = n.Mul(n, base)
+      n = n.Add(n, d)
+   }
+   nBytes := n.Bytes()
+   if len(nBytes) < 16 {
+      paddingBytes := make([]byte, 16-len(nBytes))
+      nBytes = append(paddingBytes, nBytes...)
+   }
+   return nBytes
 }
 
 func generateDeviceId(name string) string {
    hash := sha1.Sum([]byte(name))
    return base64.StdEncoding.EncodeToString(hash[:])
-}
-
-func httpGetHeaders(link string, headers map[string]string) (*http.Response, error) {
-	client := &http.Client{}
-	request, err := http.NewRequest("GET", link, nil)
-	if err != nil {
-		return empty_HTTP_RESPONSE, err
-	}
-
-	for key, value := range headers {
-		request.Header.Set(key, value)
-	}
-
-	response, err := client.Do(request)
-	if err != nil {
-		return empty_HTTP_RESPONSE, err
-	}
-
-	return response, nil
 }
 
 func powm(base, exp, modulus *big.Int) *big.Int {
@@ -179,10 +150,12 @@ type sharedKeys struct {
 func makeHelloMessage(publicKey []byte, nonce []byte) []byte {
    hello := &pb.ClientHello{
       BuildInfo: &pb.BuildInfo{
-         // CHANGE THIS TO MAKE LIBRESPOT WORK WITH FREE ACCOUNTS
-         Product:      pb.Product_PRODUCT_CLIENT.Enum(),
-         ProductFlags: []pb.ProductFlags{pb.ProductFlags_PRODUCT_FLAG_NONE},
          Platform:     pb.Platform_PLATFORM_LINUX_X86_64.Enum(),
+         // authentication failed: PremiumAccountRequired
+         // Product: pb.Product_PRODUCT_PARTNER.Enum(),
+         // CHANGE THIS TO MAKE LIBRESPOT WORK WITH FREE ACCOUNTS
+         Product: pb.Product_PRODUCT_CLIENT.Enum(),
+         ProductFlags: []pb.ProductFlags{pb.ProductFlags_PRODUCT_FLAG_NONE},
          Version:      proto.Uint64(0x10800000000),
       },
       FingerprintsSupported: []pb.Fingerprint{},
--- readme.md
+++ readme.md
@@ -9,11 +9,34 @@ audio-fa.scdn.co/audio
 ~~~
 
 - https://apkpure.com/spotify-music-i/com.spotify.music
-- https://github.com/Rafiuth/Soggfy
 - https://github.com/chris124567/spotrip
+- https://github.com/jpochyla/psst/issues/122
 - https://github.com/librespot-org/librespot/blob/dev/audio/src/decrypt.rs
-- https://github.com/meik97/XSpotify
-- https://gitlab.com/fuck-capitalism/spotifykeydumper
-- https://jfokus.se/jfokus19-preso/Adopting-gRPC-at-Spotify.pdf
-- https://news.ycombinator.com/item?id=25017167
-- https://stackoverflow.com/questions/42819550/what-is-the-hm-protocol
+
+## LoginCryptoHelloUnion
+
+- https://github.com/RoflCopter24/librespot-golang
+- https://github.com/RustedBones/sc4s
+- https://github.com/aliveoutside/LibrespotCsharp
+- https://github.com/anisse/librespot-golang
+- https://github.com/badfortrains/spotcontrol
+- https://github.com/boudabass/librespot
+- https://github.com/chatoooo/gotify
+- https://github.com/chfanghr/librespot
+- https://github.com/christosk92/SpotifyLibV2
+- https://github.com/dmfutcher/spotify-connect-scrobbler
+- https://github.com/ejurgensen/librespot-c
+- https://github.com/feelfreelinux/cspot
+- https://github.com/fherrera124/librespot
+- https://github.com/fuwwy/FolfPlayer
+- https://github.com/jpochyla/psst
+- https://github.com/jsyqrt/psotify
+- https://github.com/kokarare1212/librespot-python
+- https://github.com/librespot-org/librespot-golang
+- https://github.com/librespot-org/librespot-java
+- https://github.com/lucatk/neats
+- https://github.com/owntone/owntone-server
+- https://github.com/philippe44/librespot-spotty
+- https://github.com/plietar/librespot
+- https://github.com/pymedia/pyspotify2
+- https://github.com/tarik0/librespot-python-private
